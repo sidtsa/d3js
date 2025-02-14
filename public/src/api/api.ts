5f15a12d2b33b60e56cac1b6026589b95fa40921
sidtsa <146950854+sidtsa@users.noreply.github.com>
2025-02-14 13:54:53 +0530

    Add files via upload

---
+++ public/src/api/api.ts
@@ -0,0 +1,400 @@
+import { AskRequest, AskResponse, ChatRequest, GetApiResponse, EdaResponse, Approaches } from "./models";
+
+export async function askApi(options: AskRequest): Promise<AskResponse> {
+    const response = await fetch("/ask", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            question: options.question,
+            approach: options.approach,
+            overrides: {
+                semantic_ranker: options.overrides?.semanticRanker,
+                semantic_captions: options.overrides?.semanticCaptions,
+                top: options.overrides?.top,
+                temperature: options.overrides?.temperature,
+                prompt_template: options.overrides?.promptTemplate,
+                prompt_template_prefix: options.overrides?.promptTemplatePrefix,
+                prompt_template_suffix: options.overrides?.promptTemplateSuffix,
+                exclude_category: options.overrides?.excludeCategory
+            }
+        })
+    });
+
+    const parsedResponse: AskResponse = await response.json();
+    if (response.status > 299 || !response.ok) {
+        throw Error(parsedResponse.error || "Unknown error");
+    }
+
+    return parsedResponse;
+}
+
+export async function catalogueapi(options: ChatRequest): Promise<AskResponse> {
+    const response = await fetch("/catalogue", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            history: options.history,
+            approach: options.approach,
+            overrides: {
+                semantic_ranker: options.overrides?.semanticRanker,
+                semantic_captions: options.overrides?.semanticCaptions,
+                top: options.overrides?.top,
+                temperature: options.overrides?.temperature,
+                prompt_template: options.overrides?.promptTemplate,
+                prompt_template_prefix: options.overrides?.promptTemplatePrefix,
+                prompt_template_suffix: options.overrides?.promptTemplateSuffix,
+                exclude_category: options.overrides?.excludeCategory,
+                suggest_followup_questions: options.overrides?.suggestFollowupQuestions
+            }
+        })
+    });
+
+    const parsedResponse: AskResponse = await response.json();
+    if (response.status > 299 || !response.ok) {
+        throw Error(parsedResponse.error || "Unknown error");
+    }
+
+    return parsedResponse;
+}
+
+export async function qlikapi(options: ChatRequest): Promise<AskResponse> {
+    const response = await fetch("/qlik", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            history: options.history,
+            approach: options.approach,
+            overrides: {
+                semantic_ranker: options.overrides?.semanticRanker,
+                semantic_captions: options.overrides?.semanticCaptions,
+                top: options.overrides?.top,
+                temperature: options.overrides?.temperature,
+                prompt_template: options.overrides?.promptTemplate,
+                prompt_template_prefix: options.overrides?.promptTemplatePrefix,
+                prompt_template_suffix: options.overrides?.promptTemplateSuffix,
+                exclude_category: options.overrides?.excludeCategory,
+                suggest_followup_questions: options.overrides?.suggestFollowupQuestions
+            }
+        })
+    });
+
+    const parsedResponse: AskResponse = await response.json();
+    if (response.status > 299 || !response.ok) {
+        throw Error(parsedResponse.error || "Unknown error");
+    }
+
+    return parsedResponse;
+}
+
+export async function takedaapi(options: ChatRequest): Promise<AskResponse> {
+    const response = await fetch("/takeda", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            history: options.history,
+            approach: options.approach,
+            overrides: {
+                semantic_ranker: options.overrides?.semanticRanker,
+                semantic_captions: options.overrides?.semanticCaptions,
+                top: options.overrides?.top,
+                temperature: options.overrides?.temperature,
+                prompt_template: options.overrides?.promptTemplate,
+                prompt_template_prefix: options.overrides?.promptTemplatePrefix,
+                prompt_template_suffix: options.overrides?.promptTemplateSuffix,
+                exclude_category: options.overrides?.excludeCategory,
+                suggest_followup_questions: options.overrides?.suggestFollowupQuestions
+            }
+        })
+    });
+
+    const parsedResponse: AskResponse = await response.json();
+    if (response.status > 299 || !response.ok) {
+        throw Error(parsedResponse.error || "Unknown error");
+    }
+
+    return parsedResponse;
+}
+
+export async function chatApi(options: ChatRequest): Promise<AskResponse> {
+    const response = await fetch("/default/gpt", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            history: options.history,
+            approach: options.approach,
+            overrides: {
+                semantic_ranker: options.overrides?.semanticRanker,
+                semantic_captions: options.overrides?.semanticCaptions,
+                top: options.overrides?.top,
+                temperature: options.overrides?.temperature,
+                prompt_template: options.overrides?.promptTemplate,
+                prompt_template_prefix: options.overrides?.promptTemplatePrefix,
+                prompt_template_suffix: options.overrides?.promptTemplateSuffix,
+                exclude_category: options.overrides?.excludeCategory,
+                suggest_followup_questions: options.overrides?.suggestFollowupQuestions
+            }
+        })
+    });
+
+    const parsedResponse: AskResponse = await response.json();
+    if (response.status > 299 || !response.ok) {
+        throw Error(parsedResponse.error || "Unknown error");
+    }
+
+    return parsedResponse;
+}
+
+export async function selfserviceApi(options: ChatRequest): Promise<AskResponse> {
+    const response = await fetch("/selfservice", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            history: options.history,
+            approach: options.approach,
+            overrides: {
+                semantic_ranker: options.overrides?.semanticRanker,
+                semantic_captions: options.overrides?.semanticCaptions,
+                top: options.overrides?.top,
+                temperature: options.overrides?.temperature,
+                prompt_template: options.overrides?.promptTemplate,
+                prompt_template_prefix: options.overrides?.promptTemplatePrefix,
+                prompt_template_suffix: options.overrides?.promptTemplateSuffix,
+                exclude_category: options.overrides?.excludeCategory,
+                suggest_followup_questions: options.overrides?.suggestFollowupQuestions
+            }
+        })
+    });
+
+    const parsedResponse: AskResponse = await response.json();
+    if (response.status > 299 || !response.ok) {
+        throw Error(parsedResponse.error || "Unknown error");
+    }
+
+    return parsedResponse;
+}
+
+export async function emailApi(options: ChatRequest): Promise<AskResponse> {
+    var api = "";
+    if (options.approach === Approaches.Email) {
+        api = "/emailAssistant";
+    } else if (options.approach === Approaches.Joiner) {
+        api = "/leaver";
+    } else {
+        api = "/ct";
+    }
+    const response = await fetch(api, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            history: options.history,
+            approach: options.approach,
+            overrides: {
+                semantic_ranker: options.overrides?.semanticRanker,
+                semantic_captions: options.overrides?.semanticCaptions,
+                top: options.overrides?.top,
+                temperature: options.overrides?.temperature,
+                prompt_template: options.overrides?.promptTemplate,
+                prompt_template_prefix: options.overrides?.promptTemplatePrefix,
+                prompt_template_suffix: options.overrides?.promptTemplateSuffix,
+                exclude_category: options.overrides?.excludeCategory,
+                suggest_followup_questions: options.overrides?.suggestFollowupQuestions
+            }
+        })
+    });
+
+    const parsedResponse: AskResponse = await response.json();
+    if (response.status > 299 || !response.ok) {
+        throw Error(parsedResponse.error || "Unknown error");
+    }
+
+    return parsedResponse;
+}
+
+export async function tunedApi(options: ChatRequest): Promise<AskResponse> {
+    const response = await fetch("/tunedgpt", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            history: options.history,
+            approach: options.approach,
+            overrides: {
+                semantic_ranker: options.overrides?.semanticRanker,
+                semantic_captions: options.overrides?.semanticCaptions,
+                top: options.overrides?.top,
+                temperature: options.overrides?.temperature,
+                prompt_template: options.overrides?.promptTemplate,
+                prompt_template_prefix: options.overrides?.promptTemplatePrefix,
+                prompt_template_suffix: options.overrides?.promptTemplateSuffix,
+                exclude_category: options.overrides?.excludeCategory,
+                suggest_followup_questions: options.overrides?.suggestFollowupQuestions
+            }
+        })
+    });
+
+    const parsedResponse: AskResponse = await response.json();
+    if (response.status > 299 || !response.ok) {
+        throw Error(parsedResponse.error || "Unknown error");
+    }
+
+    return parsedResponse;
+}
+
+export async function metadataSetApi(): Promise<GetApiResponse> {
+    const response = await fetch("/db/metadataSet", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    const parsedResponse: GetApiResponse = await response.json();
+    if (response.status > 299 || !response.ok) {
+        throw Error("Unknown error");
+    }
+    return parsedResponse;
+}
+
+export async function biApi(options: ChatRequest): Promise<AskResponse> {
+    const response = await fetch("/bi", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            history: options.history,
+            approach: options.approach,
+            overrides: {
+                semantic_ranker: options.overrides?.semanticRanker,
+                semantic_captions: options.overrides?.semanticCaptions,
+                top: options.overrides?.top,
+                temperature: options.overrides?.temperature,
+                prompt_template: options.overrides?.promptTemplate,
+                prompt_template_prefix: options.overrides?.promptTemplatePrefix,
+                prompt_template_suffix: options.overrides?.promptTemplateSuffix,
+                exclude_category: options.overrides?.excludeCategory,
+                suggest_followup_questions: options.overrides?.suggestFollowupQuestions
+            }
+        })
+    });
+
+    const parsedResponse: AskResponse = await response.json();
+    if (response.status > 299 || !response.ok) {
+        throw Error(parsedResponse.error || "Unknown error");
+    }
+
+    return parsedResponse;
+}
+
+export async function metadataInfoApi(): Promise<GetApiResponse> {
+    const response = await fetch("/db/metadataInfo", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    const parsedResponse: GetApiResponse = await response.json();
+    if (response.status > 299 || !response.ok) {
+        throw Error("Unknown error");
+    }
+
+    return parsedResponse;
+}
+
+export async function logout(): Promise<GetApiResponse> {
+    const response = await fetch("/logout", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    const parsedResponse: GetApiResponse = await response.json();
+    if (response.status > 299 || !response.ok) {
+        throw Error("Unknown error");
+    }
+
+    return parsedResponse;
+}
+
+export async function edaApi(options: ChatRequest): Promise<EdaResponse> {
+    const response = await fetch("/eda", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            history: options.history,
+            approach: options.approach,
+            overrides: {
+                semantic_ranker: options.overrides?.semanticRanker,
+                semantic_captions: options.overrides?.semanticCaptions,
+                top: options.overrides?.top,
+                temperature: options.overrides?.temperature,
+                prompt_template: options.overrides?.promptTemplate,
+                prompt_template_prefix: options.overrides?.promptTemplatePrefix,
+                prompt_template_suffix: options.overrides?.promptTemplateSuffix,
+                exclude_category: options.overrides?.excludeCategory,
+                suggest_followup_questions: options.overrides?.suggestFollowupQuestions
+            }
+        })
+    });
+
+    const parsedResponse: EdaResponse = await response.json();
+    if (response.status > 299 || !response.ok) {
+        throw Error(parsedResponse.error || "Unknown error");
+    }
+
+    return parsedResponse;
+}
+
+export async function expApi(options: ChatRequest): Promise<EdaResponse> {
+    const response = await fetch("/exp", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            history: options.history,
+            approach: options.approach,
+            overrides: {
+                semantic_ranker: options.overrides?.semanticRanker,
+                semantic_captions: options.overrides?.semanticCaptions,
+                top: options.overrides?.top,
+                temperature: options.overrides?.temperature,
+                prompt_template: options.overrides?.promptTemplate,
+                prompt_template_prefix: options.overrides?.promptTemplatePrefix,
+                prompt_template_suffix: options.overrides?.promptTemplateSuffix,
+                exclude_category: options.overrides?.excludeCategory,
+                suggest_followup_questions: options.overrides?.suggestFollowupQuestions
+            }
+        })
+    });
+
+    const parsedResponse: EdaResponse = await response.json();
+    if (response.status > 299 || !response.ok) {
+        throw Error(parsedResponse.error || "Unknown error");
+    }
+
+    return parsedResponse;
+}
+
+export function getCitationFilePath(citation: string): string {
+    return `/content/${citation}`;
+}
